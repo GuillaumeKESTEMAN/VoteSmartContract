e369012bba57c7ee53ad4c82bc9a476e67a87549
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2025-03-27 09:48:27 +0100

    init

---
+++ Voting.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.29;
+
+/**
+ * @title Voting
+ */
+contract Voting {
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted(address voter, uint proposalId);
+
+
+   mapping(uint => Proposal) public proposals;
+}
