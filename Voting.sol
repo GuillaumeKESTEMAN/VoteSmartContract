09e7aae244a657e9b7e102b0134872d620d1a7a2
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2025-03-27 09:53:41 +0100

    feat: add constructor and ownable

--- Voting.sol
+++ Voting.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.29;
+pragma solidity >=0.8.29;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title Voting
  */
-contract Voting {
+contract Voting is Ownable {
     struct Voter {
         bool isRegistered;
         bool hasVoted;
-        uint votedProposalId;
+        uint256 votedProposalId;
     }
 
     struct Proposal {
         string description;
-        uint voteCount;
+        uint256 voteCount;
     }
 
     enum WorkflowStatus {
@@ -27,10 +29,14 @@ contract Voting {
     }
 
     event VoterRegistered(address voterAddress);
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-    event ProposalRegistered(uint proposalId);
-    event Voted(address voter, uint proposalId);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
 
+    mapping(uint256 => Proposal) public proposals;
 
-   mapping(uint => Proposal) public proposals;
+    constructor() Ownable(msg.sender) {}
 }
